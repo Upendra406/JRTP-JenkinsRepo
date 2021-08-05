b58076de0951296c61da337857cb9b6cf51ad1e0
Upendra406 <bakthulaupendra@gmail.com>
2021-08-05 21:35:31 +0530

    Jenkins commit

---
+++ src/main/java/com/upendra/beans/JenkinsDemo.java
@@ -0,0 +1,13 @@
+package com.upendra.beans;
+
+public class JenkinsDemo {
+	
+	public static void main(String[] args) {
+		
+		for(int i = 0; i<=10; i++) {
+			System.out.println(i);
+		}
+	}
+
+
+}
